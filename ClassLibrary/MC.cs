76988ef2551c924886d57e5a5ba36e04e6f0673d
Dominik Janos Hasko <domi1006@edu.easj.dk>
2020-09-20 12:21:47 +0200

    Task3 finished) - Unnecessary class1 removed. - MC class created with properties and methods. - MCTests class is created to test methods of MC class.

---
+++ ClassLibrary/MC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// This class represents a MotorCycle. It has a LicencePlate and a Date property and a Price and a Type method.
+    /// </summary>
+    public class MC
+    {
+        /// <summary>
+        /// The license plate of the vehicle.
+        /// </summary>
+        public string LicensePlate { get; set; }
+
+        /// <summary>
+        /// ?
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// A method that returns the price of the ticket. It takes no parameters.
+        /// </summary>
+        /// <returns>a double value, the price of the ticket.</returns>
+        public double Price() => 125;
+
+        /// <summary>
+        /// A method that returns the type of the vehicle. It takes no parameters.
+        /// </summary>
+        /// <returns>a string value, the type of the vehicle. </returns>
+        public string VehicleType() => "MC";
+    }
+}
---
+++ UnitTestTicketSystem/MCTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTicketSystem
+{
+    [TestClass]
+   public class MCTests
+    {
+        [TestMethod]
+        public void PriceTest()
+        {
+            //Arrange
+            MC motorC = new MC();
+            //Act
+            var actualPrice = motorC.Price();
+            double expectedPrice = 125;
+            //Assert
+            Assert.AreEqual(expectedPrice, actualPrice);
+        }
+
+        [TestMethod]
+        public void VehicleTypeTest()
+        {
+            //Arrange
+            MC motorC = new MC();
+            //Act
+            var actualOutput = motorC.VehicleType();
+            string expectedOutput = "MC";
+            //Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
